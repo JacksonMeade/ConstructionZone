61f77e962b48cda20f225f336a96d1edc3584e35
Jackson Meade <jacksonmeade@outlook.com>
2018-05-23 08:30:27 -0400

    v1.1.8.5

--- assets/ljs/search.js
+++ assets/ljs/search.js
@@ -7,13 +7,17 @@
 
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-        appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
+        if (item.reviewImgUrl == null) {
+          appendString += '<a href="' + item.url + '"><div class="col-md-12 testimonials-container" id="search-results"><div class="row hover-shadow"><div class="col-12"><h3>' + item.title + '</h3><p style="color:black;">' + item.content + '...</p></div></div></div></a>'
+        }
+        else {
+          appendString += '<a href="' + item.url + '"><div class="col-md-12 testimonials-container" id="search-results"><div class="row hover-shadow"><div class="col-md-3 col-sm-12"><img src="' + item.reviewImgUrl + '"></div><div class="col-md-9 col-sm-12"><h3>' + item.title + '</h3><p style="color:black;">' + item.content.substring(0,150) + '...</p></div></div></div></a>';
+        }
       }
 
       searchResults.innerHTML = appendString;
     } else {
-      searchResults.innerHTML = '<div style="background: #f2f2f2; margin-bottom: 1rem; padding: 1rem;"><div class="user row"><div class="col-lg-3 col-md-4"><div class="user_image"><img src="assets/images/face3.jpg"></div></div><h1 class="testimonials-caption col-lg-9 col-md-8"><div class="user_name custom-bold custom-fonts-style align-left pt-3 display-7">Oops.</h1><div class="user_desk custom-light custom-fonts-style align-left pt-2 display-7">Sorry, we couldn&apos;t find anything matching that description.</div></div></div></div>';
+      searchResults.innerHTML = '<div style="background: #f2f2f2; margin-bottom: 1rem; padding: 1rem;"><div class="user row"><div class="col-lg-3 col-md-4"></div><h1 class="testimonials-caption col-lg-9 col-md-8"><div class="user_name custom-bold custom-fonts-style align-left pt-3 display-7">Oops.</h1><div class="user_desk custom-light custom-fonts-style align-left pt-2 display-7">Sorry, we couldn&apos;t find anything matching that description.</div></div></div></div>';
     }
   }
 
@@ -40,20 +44,18 @@
     var idx = lunr(function () {
       this.field('id');
       this.field('title', { boost: 10 });
-      this.field('date');
+      this.field('author');
       this.field('category');
       this.field('content');
-      this.field('reviewImgUrl');
     });
 
     for (var key in window.store) { // Add the data to lunr
       idx.add({
         'id': key,
         'title': window.store[key].title,
-        'date': window.store[key].date,
+        'author': window.store[key].author,
         'category': window.store[key].category,
         'content': window.store[key].content,
-        'reviewImgUrl' : window.store[key].reviewImgUrl
       });
 
       var results = idx.search(searchTerm); // Get lunr to perform a search
